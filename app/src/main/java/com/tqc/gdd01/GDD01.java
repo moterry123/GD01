ba305ea3725abd75933cc717331b236b9dd1cd9d
moterry123 <terry23567@gmail.com>
2021-04-29 16:08:16 +0800

    gOD01

--- app/src/main/java/com/tqc/gdd01/GDD01.java
+++ app/src/main/java/com/tqc/gdd01/GDD01.java
@@ -22,28 +22,32 @@ public class GDD01 extends Activity
   public void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.main);
-    etheight = findViewById(R.id.etHeight);
-    etweight = findViewById(R.id.etWeight);
-    rb1 = findViewById(R.id.rbman);
-    rb2 = findViewById(R.id.rbwoman);
+
     final Button b1 = (Button) findViewById(R.id.button1);
     b1.setOnClickListener(new Button.OnClickListener()
     {
       public void onClick(View v)
       {
         //從輸入介面中取出了的身高、體重值，要將身高、體重值傳送給 child_Activity 後作計算
+        etheight = findViewById(R.id.tv_Height);
+        etweight = findViewById(R.id.tv_Weight);
+        rb1 = findViewById(R.id.rb_Man);
+        rb2 = findViewById(R.id.rb_Woman);
 
+        Bundle bundle = new Bundle();
+        bundle.putString("h",etheight.getText().toString());
+        bundle.putString("w",etweight.getText().toString());
+        if(rb1.isChecked()){
+          bundle.putString("s","M");
+        }else{
+          bundle.putString("s","F");
+        }
         //這些附加在 Intent 上的訊息都儲存在 Bundle 物件中
-
+        Intent intent = new Intent(GDD01.this,GDD01_child.class);
+        intent.putExtras(bundle);
         //透過「intent.putExtras(bundle)」敘述，將「bundle」 物件附加在 Intent 上，隨著 Intent 送出而送出
-
+        startActivity(intent);
       }
     });
+   }
   }
-
-  public void b1(View view) {
-    double h = Double.parseDouble(etheight.getText().toString());
-    double w = Double.parseDouble(etweight.getText().toString());
-    double bmi = w/(h*h);
-  }
-}
